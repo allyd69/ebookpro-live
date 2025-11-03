2a812b0531221a3fb6451da8c94b1dc404d5d2de
Alice Tanielu <166324500+allyd69@users.noreply.github.com>
2025-11-03 19:28:33 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
-self.addEventListener('install', e => self.skipWaiting());
+const CACHE = 'ebookpro-v1';
+const assets = ['/', 'index.html', 'manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(assets)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
